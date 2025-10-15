35cbe189833701bee385e19370eccf0101ef96fb
Syed Irtiza Jafar <stu502331@misdoha.com>
2025-10-16 00:05:33 +0300

    Update favicon and og-image to pull from GitHub repo directly

---
+++ convert-favicon.js
@@ -0,0 +1,14 @@
+// Simple conversion script - create a 512x512 favicon
+const fs = require('fs');
+
+// For now, let's just copy the SVG and create a placeholder JPG
+// You can manually convert it using an online tool like:
+// https://cloudconvert.com/svg-to-jpg
+// or https://convertio.co/svg-jpg/
+
+console.log('Please convert public/favicon.svg to JPG using:');
+console.log('1. Visit https://cloudconvert.com/svg-to-jpg');
+console.log('2. Upload public/favicon.svg');
+console.log('3. Download as favicon.jpg');
+console.log('4. Save to public/favicon.jpg');
+console.log('\nOr use an image editor like GIMP, Photoshop, or online converter.');
